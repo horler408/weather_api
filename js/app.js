81e9c25fa84573e06c5137d9ad0e6390b8548211
horler408 <horler.web@gmail.com>
2020-11-13 06:33:15 +0100

    location api call

--- js/app.js
+++ js/app.js
@@ -2,24 +2,26 @@ const latitude = document.getElementById('lat');
 const longitude = document.getElementById('lng');
 
 const BASE_URL = 'https://fcc-weather-api.glitch.me/'
-//const lat = 35
-//const lng = 139
-const lat = latitude.value
-const lng = longitude.value
-console.log(lat);
-const URL = `https://fcc-weather-api.glitch.me/api/current?lat=${lat}&lon=${lng}`;
+const proxy = "https://cors-anywhere.herokuapp.com/" 
 
-// if (navigator.geolocation) {
-//     navigator.geolocation.getCurrentPosition(position => {
-//         const lat = position.coords.latitude;
-//         const lng = position.coords.longitude;
+//const URL = `https://fcc-weather-api.glitch.me/api/current?lat=${lat}&lon=${lng}`;
 
-const weatherRequest = () => {
-    fetch(URL)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-    })
-}
-    
-weatherRequest()
+window.addEventListener('load', () => {
+    let lat;
+    let lng;
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            //console.log(position);
+            lng = position.coords.longitude;
+            lat = position.coords.latitude;
+
+            const URL = `https://fcc-weather-api.glitch.me/api/current?lat=${lat}&lon=${lng}`;
+            fetch(URL)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+            })
+        })
+    }
+})
