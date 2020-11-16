3b2456ff5e5443a07e4a510f94f046c02de0340b
horler408 <horler.web@gmail.com>
2020-11-16 20:46:59 +0100

    complete api pull

--- js/app.js
+++ js/app.js
@@ -1,15 +1,26 @@
+// Accessing Form DOM
+const form = document.querySelector(".form")
 const latitude = document.getElementById('lat');
 const longitude = document.getElementById('lng');
+// Display DOM
+const title = document.querySelector(".title");
+const tempSection = document.querySelector(".temperature-section")
 const temperature = document.querySelector('.temperature__degree');
-const unit = document.querySelector('.degree-section span');
+const unit = document.querySelector('.unit');
 const description = document.querySelector('.temperature__description')
 const img =document.querySelector(".location img");
 const timezone = document.querySelector(".location-timezone")
 
 const BASE_URL = 'https://fcc-weather-api.glitch.me/'
-const proxy = "https://cors-anywhere.herokuapp.com/" 
+const proxy = "https://cors-anywhere.herokuapp.com/"  
 
-//const URL = `https://fcc-weather-api.glitch.me/api/current?lat=${lat}&lon=${lng}`;
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    let lat = latitude.value;
+    let lng = longitude.value;
+
+    const URL = `https://fcc-weather-api.glitch.me/api/current?lat=${lat}&lon=${lng}`;
+})
 
 window.addEventListener('load', () => {
     let lat;
@@ -27,10 +38,26 @@ window.addEventListener('load', () => {
             .then(data => {
                 console.log(data);
 
-                timezone.innerHTML = data.timezone
-                temperature.innerHTML = data.main.temp_max;
-                description.innerHTML = data.weather[0].description;
+                //Set DOM from API
+                title.innerHTML = `${data.name}, ${data.sys.country}`
+                timezone.textContent = data.timezone
+                temperature.textContent = data.main.temp_max;
+                description.textContent = data.weather[0].description;
                 img.src = data.weather[0].icon
+
+                // Temperature Formula
+                let celsius = (data.main.temp - 32) * (5 / 9);
+
+                //Change of Temperature
+                tempSection.addEventListener("click", () => {
+                    if(unit.textContent === "F") {
+                        unit.textContent = "C"
+                        temperature.textContent = Math.floor(celsius); 
+                    } else {
+                        unit.textContent = "F"
+                        temperature.textContent = data.main.temp
+                    }
+                })
             })
         })
     }
